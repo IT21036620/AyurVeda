700b2cb14e8d19d786f20941d0a50579865b253c
007Miles <milendrafernando@gmail.com>
2023-04-14 00:45:12 +0530

    Implemented extra security packages

--- services/product/app.js
+++ services/product/app.js
@@ -1,5 +1,12 @@
 require('dotenv').config()
 require('express-async-errors')
+
+// extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 const express = require('express')
 const app = express()
 
@@ -16,7 +23,17 @@ const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
 //middleware
+app.set('trust proxy', 1)
+app.use(
+  rateLimiter({
+    windowsMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+)
 app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 //routes
 app.use('/api/v1/products', productRouter) // authenticateSeller, productRouter
