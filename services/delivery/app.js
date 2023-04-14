fbb30a65d9b29ff9ebd61cab6c88424e7ae30a15
miyuru-nimesh <miyurunimesh@gmail.com>
2023-04-14 19:20:23 +0530

    delivery service initial commit

---
+++ services/delivery/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const deliveries = require('./routes/delivery')
+const connectDB = require('./db/connect')
+require('dotenv').config()
+const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+//middleware
+app.use(express.json())
+
+//routes
+app.use('/api/v1/deliveries', deliveries)
+
+app.use(notFound)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 3000
+
+const start = async () => {
+    try{
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`server listening on port ${port}...`));
+    }catch(error){
+        console.log(error)
+    }
+}
+
+
+start()
