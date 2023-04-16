a09e1aa1d67831f39f62a7f01f1503929954f569
Kariyawasam K.P.W.D.V <87995561+IT21036620@users.noreply.github.com>
2023-04-16 10:29:58 +0530

    frontend structure created

---
+++ frontend/src/routes/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import BuyerRoutes from './buyerRoutes'
+
+const IndexRoutes = () => {
+  return (
+    <div>
+      <BuyerRoutes />
+    </div>
+  )
+}
+
+export default IndexRoutes
