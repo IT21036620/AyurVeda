f5ab89b421c0b538427971e2cd21c94f9853b5e5
007Miles <milendrafernando@gmail.com>
2023-04-21 21:26:48 +0530

    Modified product page

--- frontend/src/components/products/Product.js
+++ frontend/src/components/products/Product.js
@@ -1,27 +1,50 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './component.css'
+import { FaShoppingCart } from 'react-icons/fa'
 
 const Product = ({ image, product_name, id, price, rating, rate_count }) => {
+  // return (
+  //   <article className="product">
+  //     <div className="img-container">
+  //       <img
+  //         crossOrigin="anonymous"
+  //         src={`http:\/\/localhost:4000\/${image}`}
+  //         alt={product_name}
+  //       />
+  //     </div>
+  //     <div className="product-footer">
+  //       <h3>{product_name}</h3>
+  //       <h4>{rating}/5</h4>
+  //       <h6>{`(${rate_count} ratings)`}</h6>
+  //       <p>{`Rs.${price}.00`}</p>
+  //       <Link to={`/product/${id}`} className="btn btn-primary btn-details">
+  //         details
+  //       </Link>
+  //     </div>
+  //   </article>
+  // )
+
   return (
-    <article className="product">
-      <div className="img-container">
+    <div className="card">
+      <div className="card_img">
         <img
           crossOrigin="anonymous"
           src={`http:\/\/localhost:4000\/${image}`}
-          alt={product_name}
         />
       </div>
-      <div className="product-footer">
-        <h3>{product_name}</h3>
-        <h4>{rating}/5</h4>
-        <h6>{`(${rate_count} ratings)`}</h6>
-        <p>{`Rs.${price}.00`}</p>
-        <Link to={`/product/${id}`} className="btn btn-primary btn-details">
-          details
-        </Link>
+      <div className="card_header">
+        <h2 class="font-bold">{product_name}</h2>
+        <p>{rating}/5</p>
+        <p className="price">
+          {price}.00
+          <span>LKR</span>
+        </p>
+        <div class="font-sans bg-[rgb(33,190,33)] hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full">
+          Add To Cart <FaShoppingCart class="m-auto" />
+        </div>
       </div>
-    </article>
+    </div>
   )
 }
 
--- frontend/src/pages/products/Products.js
+++ frontend/src/pages/products/Products.js
@@ -2,13 +2,16 @@ import React from 'react'
 import SearchForm from '../../components/products/SearchForm'
 import ProductList from '../../components/products/ProductList'
 import { AppProvider } from './context'
+import '../../components/products/product.css'
 
 const Products = () => {
   return (
     <main>
       <AppProvider>
-        <SearchForm />
-        <ProductList />
+        <div className="container">
+          <SearchForm />
+          <ProductList />
+        </div>
       </AppProvider>
     </main>
   )
