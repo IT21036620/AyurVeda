5c78090005b022401f185d580008e6765a4f8506
Kariyawasam K.P.W.D.V <87995561+IT21036620@users.noreply.github.com>
2023-04-26 18:23:51 +0530

    email service updated

--- services/email/app.js
+++ services/email/app.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import mailRouter from './route.js'
+import cors from 'cors'
 
 dotenv.config()
 
 const app = express()
 app.use(express.json({ limit: '1mb' }))
+
+app.use(cors())
 
 app.use('/api/mail', mailRouter)
 
