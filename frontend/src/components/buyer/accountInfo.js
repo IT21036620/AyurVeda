db2fa900764fc1aaba5997aca230e084d86e9126
Kariyawasam K.P.W.D.V <87995561+IT21036620@users.noreply.github.com>
2023-05-19 09:35:00 +0530

    review funtionality implemented

--- frontend/src/components/buyer/accountInfo.js
+++ frontend/src/components/buyer/accountInfo.js
@@ -53,7 +53,7 @@ const AccountInfo = () => {
 
   return (
     <div className="bg-gray-100 p-4 rounded-lg ml-36">
-      <h2 className="text-2xl font-bold mb-4 inline-flex items-center">
+      <h2 className="text-3xl font-bold mb-4 inline-flex items-center text-blue-400">
         <FaUser className="mr-2" />
         Account Information
       </h2>
